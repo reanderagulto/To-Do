4f31680acd656489815a951e7684163a77f69188
Reander Agulto <reanderagulto29@gmail.com>
2023-04-21 21:01:10 +0800

    Pulling from Repo

--- src/components/addTodo/index.js
+++ src/components/addTodo/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import cx from './AddTodo.module.scss'
 import clsx from 'clsx'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 const AddTodo = () => {
+
+  const clickHandler = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <section className={cx.wrapper}>
       <form className={cx.form}>
         <div className={cx.group}>
-          <label for="">Add Todo</label>
+          <label htmlFor="todo">Add Todo</label>
           <input type="text" name="todo" className={cx.control}/>
         </div>
         <div className={cx.group}>
@@ -16,7 +23,7 @@ const AddTodo = () => {
             className={clsx(cx.btn, cx.submit)}
             onClick={clickHandler}
           >
-            +
+            <FontAwesomeIcon icon="fa-solid fa-plus" />
           </button>
         </div>
 
